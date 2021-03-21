d4622d30ed919a30abacbdfc123f59b7f9b0dadb
leodu <lenadtoo@gmail.com>
2021-03-21 23:52:42 +0300

    commits

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -17,7 +17,7 @@ class AuthKey
     {
         $token = $request->header('APP_KEY');
         if ($token!='123') {
-            return response()->json(['message'=>'App Key not found'],401);
+            return response()->json(['message'=>'API Key not found'],401);
             //exit();
         }
         return $next($request);
